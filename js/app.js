2d382a6c210d131ba93e5ca267ca2e0109d66549
flaviafogaca <flaviamariafogaca@gmail.com>
2022-04-07 14:55:33 -0400

    Aula 15 - DOM - parentNode

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
 console.log(
-    document.body.childNodes
+document.getElementById("ilheus").parentNode.parentNode.parentNode
 )
-
-var lista = document.body.childNodes
-alert(lista[5].childNodes[3].childNodes[0].nodeValue)
-
-//Se nodeType retorna 1:nó de elemento; se 2: nó de atributo; se 3:nó de texto; se 8: nó de comentário//
