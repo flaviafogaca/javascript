9b353613f257c38fcb60a395082fa2ee78157a1f
flaviafogaca <flaviamariafogaca@gmail.com>
2022-04-07 14:20:38 -0400

    Aula 13 - DOM - Estudando childNodes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-var p = document.createElement("p")
-p.innerHTML = "Oi gente"
-
-var img = document.createElement("img")
-img.src = "senhor-dos-aneis.jpg"
-
-document.getElementById("conteudo").appendChild(p)
-document.getElementById("conteudo").appendChild(img)
-
-document.getElementById("conteudo").removeChild(img)
+console.log(
+    document.body.childNodes
+)
