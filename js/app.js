2403fca5c8539f52f9e1bf343e280fbc6cee9946
flaviafogaca <flaviamariafogaca@gmail.com>
2022-04-07 14:40:01 -0400

    Aula 14 - DOM - estudando nós

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 console.log(
     document.body.childNodes
 )
+
+var lista = document.body.childNodes
+alert(lista[5].childNodes[3].childNodes[0].nodeValue)
+
+//Se nodeType retorna 1:nó de elemento; se 2: nó de atributo; se 3:nó de texto; se 8: nó de comentário//
