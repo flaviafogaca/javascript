a6153aa8c9a5cdac0461b69dcbb7193a8b470f71
flaviafogaca <flaviamariafogaca@gmail.com>
2022-04-07 16:14:22 -0400

    Aula 16_getAttribute/setAttribute/removeAttribute

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-console.log(
-document.getElementById("ilheus").parentNode.parentNode.parentNode
-)
+document.getElementById("btn-set").addEventListener("click", function(){
+    document.getElementById("title").setAttribute("class","red")
+})
+
+document.getElementById("btn-remove").addEventListener("click", function(){
+    document.getElementById("title").removeAttribute("class")
+})
+
+document.getElementById("btn-get").addEventListener("click", function(){
+    var value = document.getElementById("title").getAttribute("class")
+    document.getElementById("class").innerHTML = value
+})
