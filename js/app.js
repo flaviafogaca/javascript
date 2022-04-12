b064892feb15e7e99582f2832f888f01b50ce17a
flaviafogaca <flaviamariafogaca@gmail.com>
2022-04-12 10:07:50 -0400

    Aula 19 - replaceChild

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 var lista = document.getElementsByTagName("ul")[1]
 var itens = lista.children
-
 var novoItem = document.createElement("li")
 novoItem.textContent = "z"
 
 lista.insertBefore(novoItem, itens[0])
 
-console.log(itens)
+var lista2 = document.getElementsByTagName("ul")[1]
+var itens2 = lista2.children
+var novoItem2 = document.createElement("li")
+novoItem2.textContent = "y"
+
+lista.replaceChild(novoItem2, itens2[2])
+
+console.log(itens2)
